7f5a006442c49bf9c031b9b3197339c415eb6fbd
spence <spencer.d.tenney@gmail.com>
2021-03-06 14:17:52 -0500

    did most of the patterns

--- form_validation.js
+++ form_validation.js
@@ -4,40 +4,82 @@ function name_check(){
 }
 
 function email_check(){
-        if(event.target.value.search(/^[A-Z]{3}\d{3}@marietta.edu$/i)>-1){
-            alert("Please enter a valid email adress\nEx:abc123@marietta.edu");
-            event.target.value="";
-        }
+    var val=event.target.value;
+    
+    if(val.search(/^[A-Z]{3}\d{3}@marietta.edu$/)==-1){
+        alert("Please enter a valid email address\nEx:abc123@marietta.edu");
+        event.target.value="";
+    }
 }
 
 function major_check(){
-
+    var val=event.target.value;
+    
+    if(val.search(/^([A-Za-z] ?)+$/)==-1){
+        alert("");
+        event.target.value="";
+    }
 }
 
 function department_check(){
-
+    var val=event.target.value;
+    
+    if(val.search(/^([A-Za-z] ?)+$/)==-1){
+        alert("");
+        event.target.value="";
+    }
 }
 
 function course_prefix_check(){
-
+    var val=event.target.value;
+    
+    if(val.search(/^[A-Z]{3,4}$/)==-1){
+        alert("");
+        event.target.value="";
+    }
 }
 
 function course_number_check(){
-
+    var val=event.target.value;
+    
+    if(val.search(/^[0-3]\d{2}$/)==-1){
+        alert("");
+        event.target.value="";
+    }
 }
 
 function course_section_check(){
+    var val=event.target.value;
     
+    if(val.search(/^\d{2}$/i)==-1){
+        alert("");
+        event.target.value="";
+    }
 }
 
 function room_check(){
+    var val=event.target.value;
 
+    if(val.search(/^$/i)==-1){
+        alert("");
+        event.target.value="";
+    }
 }
 
 function credit_hours_check(){
+    var val=event.target.value;
 
+    if(val.search(/^[0-4]$/i)==-1){
+        alert("");
+        event.target.value="";
+    }
 }
 
 function enrollment_cap_check(){
-
+    var val=event.target.value;
+    
+    if(val.search(/^[1-9][0-9]$/i)==-1){
+        alert("");
+        event.target.value="";
+    }
 }
