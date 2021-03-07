d4ed0e4fe26f547102e9c8d179aaf64f18784ced
spence <spencer.d.tenney@gmail.com>
2021-03-07 16:42:22 -0500

    made event registration conditional to squish some errors

--- event_registration.js
+++ event_registration.js
@@ -1,26 +1,52 @@
 //this file handles registration of all events for all inputs
+//if a field exists, then it will attach the appropriate event
 
 //common fields
 //name validation
-document.getElementById("first_name").onchange=name_check;
-document.getElementById("last_name").onchange=name_check;
+if(document.getElementById("first_name")){
+	document.getElementById("first_name").onchange=name_check;
+}
+if(document.getElementById("last_name")){
+	document.getElementById("last_name").onchange=name_check;
+}
 
 //email validation
-document.getElementById("email").onchange=email_check;
+if(document.getElementById("email")){
+	document.getElementById("email").onchange=email_check;
+}
 
 //student specific fields
-document.getElementById("major").onchange=major_check;
+if(document.getElementById("major")){
+	document.getElementById("major").onchange=major_check;
+}
 
 //instructor specific fields
-document.getElementById("department").onchange=department_check;
+if (document.getElementById("department")){
+    document.getElementById("department").onchange=department_check;
+}
+
 
 //course specific fields
-document.getElementById("course_prefix").onchange=course_prefix_check;
-document.getElementById("course_number").onchange=course_number_check;
-document.getElementById("course_section").onchange=course_section_check;
-document.getElementById("room").onchange=room_check;
-document.getElementById("credit_hours").onchange=credit_hours_check;
-document.getElementById("enrollment_cap").onchange=enrollment_cap_check;
+if(document.getElementById("course_prefix")){
+	document.getElementById("course_prefix").onchange=course_prefix_check;
+}
+if(document.getElementById("course_number")){
+	document.getElementById("course_number").onchange=course_number_check;
+}
+if(document.getElementById("course_section")){
+	document.getElementById("course_section").onchange=course_section_check;
+}
+if(document.getElementById("room")){
+	document.getElementById("room").onchange=room_check;
+}
+if(document.getElementById("credit_hours")){
+	document.getElementById("credit_hours").onchange=credit_hours_check;
+}
+if(document.getElementById("enrollment_cap")){
+	document.getElementById("enrollment_cap").onchange=enrollment_cap_check;
+}
 
 //submit button onclick event
-document.getElementById("submit").onclick=submit_output;
+if(document.getElementById("submit")){
+	document.getElementById("submit").onclick=display_form;
+}
