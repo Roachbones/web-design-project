994fa4ddc0b9fbe65ca903bb257a58f3c80dc79c
Tony Ruyack <tonterr12@gmail.com>
2021-03-28 18:03:44 -0400

    added unique names to class time dropdowns

--- add_course.php
+++ add_course.php
@@ -55,7 +55,7 @@
                 </select>
                 
                 <label for="Monday_Wednesday_Friday_times" id="mwf_label">time</label>
-                <select name="time" id="Monday_Wednesday_Friday">
+                <select name="time1" id="Monday_Wednesday_Friday">
                     <option value="8:00 AM to 8:50 AM">8:00 AM to 8:50 AM</option>
                     <option value="9:00 AM to 9:50 AM">9:00 AM to 9:50 AM</option>
                     <option value="10:00 AM to 10:50 AM">10:00 AM to 10:50 AM</option>
@@ -66,7 +66,7 @@
                     <option value="3:00 PM to 3:50 PM">3:00 PM to 3:50 PM</option>
                 </select>
                 <label for="Tuesday_Thursday_times" id="tth_label" class="hidden">time</label>
-                <select name="time" id="Tuesday_Thursday" class="hidden">
+                <select name="time2" id="Tuesday_Thursday" class="hidden">
                     <option value="8:00 AM to 9:15 AM">8:00 AM to 9:15 AM</option>
                     <option value="9:30 AM to 10:45 AM">9:30 AM to 10:45 AM</option>
                     <option value="11:10 AM to 12:15 PM">11:10 AM to 12:15 PM</option>
@@ -74,7 +74,7 @@
                     <option value="2:30 PM to 3:45 PM">2:30 PM to 3:45 PM</option>
                 </select>
                 <label for="any_single_day_times" id="singleday_label" class="hidden">time</label>
-                <select name="time" id="any_single_day" class="hidden">
+                <select name="time3" id="any_single_day" class="hidden">
                     <option value="1:00 PM to 4:00 PM">1:00 PM to 4:00 PM</option>
                     <option value="2:00 PM to 5:00 PM">2:00 PM to 5:00 PM</option>
                     <option value="7:00 PM to 9:30 PM">7:00 PM to 9:30 PM</option>
