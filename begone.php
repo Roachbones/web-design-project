c61ff3039515b2f6aef4d1e965c6d44de747dce3
spence <spencer.d.tenney@gmail.com>
2021-04-01 19:55:12 -0400

    fixed 403 h lookin wack

--- begone.php
+++ begone.php
@@ -1,2 +1,2 @@
-            <h2> 403 forbidden >:( </h2>
+            <h1> 403 forbidden >:( </h1>
             <p> you awful, vile, heinous fool! you are <b>forbidden</b> from accessing this page! <a href="/index.php">BEGONE!!!!</a>
