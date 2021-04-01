eda323596157c5242bd55de9febac48317f81d92
Tony Ruyack <tonterr12@gmail.com>
2021-04-01 17:55:04 -0400

    trying loop file solution to displaying tables

---
+++ add_instructor_confirmation.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="author" content="the cool web design group">
+<meta name="description" content="Boring course registration project for Dr. Williamson's Web Design & Programming class, spring 2021.">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- mobile compatibility -->
+<title>Add Instructor Confirmation</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script src="form_validation.js"></script>
+<script src="display_form.js"></script>
+<?php session_start(); ?>
+<?php include("login_check.php"); ?>
+
+</head>
+<body>
+    <header>
+        <?php include("header.php"); ?>
+    </header>
+    <h1>Add Instructor Confirmation</h1>
+<!--
+
+
+//assign values of all form inputs to variables
+$firstName= $_POST["first_name"];
+$lastName = $_POST["last_name"];
+$name = $firstName." ".$lastName;
+
+$cdepartment = $_POST["department"];
+$rank = $_POST["rank"];
+$email = $_POST["email"];
+
+//check if the non required fields exist or not
+if (isset($courseName))
+    $courseName = "N/A";
+if (isset($name))
+    $name = "N/A";
+
+<h3>The following course will be added</h3>
+<table>
+    <tr>
+        <td>Course Year</td><td> <?php print "$courseYear";?> </td>
+    </tr>
+
+    <tr>
+        <td>Course</td><td> <?php print "$course";?> </td>
+    </tr>
+
+    <tr>
+        <td>Course Name</td><td> <?php print "$courseName";?> </td>
+    </tr>
+
+    <tr>
+        <td>Room</td> <td> <?php print "$room"; ?> </td>
+    </tr>
+
+    <tr>
+        <td>Days</td><td> <?php print "$days"; ?> </td>
+    </tr>
+
+    <tr>
+        <td>Time</td><td> <?php print "$time"; ?> </td>
+    </tr>
+    <tr>
+        <td>Credit Hours</td> <td> <?php print "$creditHours"; ?> </td>
+    </tr>
+
+    <tr>
+        <td>Instructor</td> <td> <?php print "$instructor"; ?> </td>
+    </tr>
+
+    <tr>
+        <td>Enrollment Cap</td> <td> <?php print "$enrollmentCap"; ?> </td>
+    </tr>
+</table>
+-->
+<h3>The following instructor will be added</h3>
+<table>
+<?php include("confirmation_loop.php");?>
+</table>
+</body>
+</html>
---
+++ confirmation_loop.php
@@ -0,0 +1,25 @@
+<?php
+$arr = $_POST;
+// if $_POST has the days dropdown, remove the hidden time dropdowns from the array
+if(isset($arr["days"])){
+        if ($arr["days"] == "Monday, Wednesday, Friday"){
+                unset($arr["time2"]);
+                unset($arr["time3"]);
+        }
+        
+        elseif ($arr["days"] == "Tuesday, Thursday"){
+                unset($arr["time1"]);
+                unset($arr["time3"]);
+        }
+        else{
+                unset($arr["time1"]);
+                unset($arr["time2"]);
+        }
+}       
+
+foreach ($arr as $key => $value){
+        
+        echo "<tr><td>$key</td>";
+        echo "<td>$value</td></tr>";
+}
+?>
