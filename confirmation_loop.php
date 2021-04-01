339f4d27cea5b756aff7b4e05706336029c7ed48
Tony Ruyack <tonterr12@gmail.com>
2021-04-01 18:37:57 -0400

    fixed time display in table

--- confirmation_loop.php
+++ confirmation_loop.php
@@ -2,21 +2,20 @@
 $arr = $_POST;
 // if $_POST has the days dropdown, remove the hidden time dropdowns from the array
 if(isset($arr["days"])){
-        if ($arr["days"] == "Monday, Wednesday, Friday"){
-                unset($arr["time2"]);
-                unset($arr["time3"]);
-        }
+        if ($arr["days"] == "Monday, Wednesday, Friday")
+                $arr["time"] = $arr["time1"];
         
-        elseif ($arr["days"] == "Tuesday, Thursday"){
-                unset($arr["time1"]);
-                unset($arr["time3"]);
-        }
-        else{
-                unset($arr["time1"]);
-                unset($arr["time2"]);
-        }
+        elseif ($arr["days"] == "Tuesday, Thursday")
+                $arr["time"] = $arr["time2"];
+        
+        else
+                $arr["time"] = $arr["time3"];
+
 }       
 
+unset($arr["time1"]);
+unset($arr["time2"]);
+unset($arr["time3"]);
 foreach ($arr as $key => $value){
         if(!isset($arr[$key]))
                 $arr[$key] = "N/A";
