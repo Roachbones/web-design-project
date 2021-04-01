43305d7a32e15f498a932a61cd557c50c1079bc7
spence <spencer.d.tenney@gmail.com>
2021-04-01 18:58:27 -0400

    fixed time field and unserscores showing in keys

--- confirmation_loop.php
+++ confirmation_loop.php
@@ -2,24 +2,23 @@
 $arr = $_POST;
 // if $_POST has the days dropdown, remove the hidden time dropdowns from the array
 if(isset($arr["days"])){
-        if ($arr["days"] == "Monday, Wednesday, Friday")
-                $arr["time"] = $arr["time1"];
-        
-        elseif ($arr["days"] == "Tuesday, Thursday")
-                $arr["time"] = $arr["time2"];
-        
-        else
-                $arr["time"] = $arr["time3"];
-
+        if ($arr["days"] == "Monday, Wednesday, Friday"){
+                unset($arr["time2"]);
+                unset($arr["time3"]);
+        }
+        elseif ($arr["days"] == "Tuesday, Thursday"){
+                unset($arr["time1"]);
+                unset($arr["time3"]);
+        }
+        else{
+                unset($arr["time1"]);
+                unset($arr["time2"]);
+        }
 }       
 
-unset($arr["time1"]);
-unset($arr["time2"]);
-unset($arr["time3"]);
 foreach ($arr as $key => $value){
-        if(!isset($arr[$key]))
-                $arr[$key] = "N/A";
-        str_replace("_", " ", $key);
+        $key=str_replace("_", " ", $key);
+        $key=preg_replace("/\d/", "", $key);
         echo "<tr><td>$key</td>";
         echo "<td>$value</td></tr>";
 }
