7fc367334b5da6ffe522264dc168fcc78855d677
Tony Ruyack <tonterr12@gmail.com>
2021-04-01 18:59:43 -0400

    added confirmation files for remaining pages

---
+++ enroll_student_confirmation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="author" content="the cool web design group">
+<meta name="description" content="Boring course registration project for Dr. Williamson's Web Design & Programming class, spring 2021.">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- mobile compatibility -->
+<title>Enroll Student Confirmation</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script src="form_validation.js"></script>
+<?php session_start(); ?>
+<?php include("login_check.php"); ?>
+
+</head>
+<body>
+    <header>
+        <?php include("header.php"); ?>
+    </header>
+    <h1>Enroll Student Confirmation</h1>   
+<h3> <? echo"$_POST["first_name"].' '.$_POST["last_name"]"; ?> will be enrolled</h3>"
+<table>
+    <?php include("confirmation_loop.php");?>
+</table>
+</body>
+</html>
