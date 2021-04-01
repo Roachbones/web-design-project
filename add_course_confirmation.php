4c0ad5b3a65fa1302688e677e4bf8dbc8bc13990
Tony Ruyack <tonterr12@gmail.com>
2021-04-01 18:22:51 -0400

    put loop solution in add_course

--- add_course_confirmation.php
+++ add_course_confirmation.php
@@ -17,74 +17,10 @@
     <header>
         <?php include("header.php"); ?>
     </header>
-    <h1>Add Course Confirmation</h1>
-<?php
-
-//assign values of all form inputs to variables
-$courseYear = $_POST["course_year"];
-$coursePrefix = $_POST["course_prefix"];
-$courseNumber = $_POST["course_number"];
-$courseSection = $_POST["course_section"];
-$course = $coursePrefix." ".$courseNumber."-".$courseSection;
-$courseName = $_POST["course_name"];
-$room = $_POST["room"];
-$days = $_POST["days"];
-if ($days == "Monday, Wednesday, Friday")
-    $time = $_POST["time1"];
-elseif ($days == "Tuesday, Thursday")
-    $time = $_POST["time2"];
-else
-    $time = $_POST["time3"];
-
-$creditHours = $_POST["credit_hours"];
-$instructorFirstName = $_POST["instructor_first_name"];
-$instructorLastName = $_POST["instructor_last_name"];
-$instructor = $instructorFirstName." ".$instructorLastName;
-$enrollmentCap = $_POST["enrollment_cap"];
-
-//check if the non required fields exist or not
-if (!isset($courseName))
-    $courseName = "N/A";
-if (!isset($name))
-    $name = "N/A";
-?>
+    <h1>Add Course Confirmation</h1>   
 <h3>The following course will be added</h3>
 <table>
-    <tr>
-        <td>Course Year</td><td> <?php print "$courseYear";?> </td>
-    </tr>
-
-    <tr>
-        <td>Course</td><td> <?php print "$course";?> </td>
-    </tr>
-
-    <tr>
-        <td>Course Name</td><td> <?php print "$courseName";?> </td>
-    </tr>
-
-    <tr>
-        <td>Room</td> <td> <?php print "$room"; ?> </td>
-    </tr>
-
-    <tr>
-        <td>Days</td><td> <?php print "$days"; ?> </td>
-    </tr>
-
-    <tr>
-        <td>Time</td><td> <?php print "$time"; ?> </td>
-    </tr>
-    <tr>
-        <td>Credit Hours</td> <td> <?php print "$creditHours"; ?> </td>
-    </tr>
-
-    <tr>
-        <td>Instructor</td> <td> <?php print "$instructor"; ?> </td>
-    </tr>
-
-    <tr>
-        <td>Enrollment Cap</td> <td> <?php print "$enrollmentCap"; ?> </td>
-    </tr>
+    <?php include("confirmation_loop.php");?>
 </table>
-
 </body>
 </html>
--- add_instructor_confirmation.php
+++ add_instructor_confirmation.php
@@ -17,66 +17,10 @@
     <header>
         <?php include("header.php"); ?>
     </header>
-    <h1>Add Instructor Confirmation</h1>
-<!--
-
-
-//assign values of all form inputs to variables
-$firstName= $_POST["first_name"];
-$lastName = $_POST["last_name"];
-$name = $firstName." ".$lastName;
-
-$cdepartment = $_POST["department"];
-$rank = $_POST["rank"];
-$email = $_POST["email"];
-
-//check if the non required fields exist or not
-if (isset($courseName))
-    $courseName = "N/A";
-if (isset($name))
-    $name = "N/A";
-
-<h3>The following course will be added</h3>
-<table>
-    <tr>
-        <td>Course Year</td><td> <?php print "$courseYear";?> </td>
-    </tr>
-
-    <tr>
-        <td>Course</td><td> <?php print "$course";?> </td>
-    </tr>
-
-    <tr>
-        <td>Course Name</td><td> <?php print "$courseName";?> </td>
-    </tr>
-
-    <tr>
-        <td>Room</td> <td> <?php print "$room"; ?> </td>
-    </tr>
-
-    <tr>
-        <td>Days</td><td> <?php print "$days"; ?> </td>
-    </tr>
-
-    <tr>
-        <td>Time</td><td> <?php print "$time"; ?> </td>
-    </tr>
-    <tr>
-        <td>Credit Hours</td> <td> <?php print "$creditHours"; ?> </td>
-    </tr>
-
-    <tr>
-        <td>Instructor</td> <td> <?php print "$instructor"; ?> </td>
-    </tr>
-
-    <tr>
-        <td>Enrollment Cap</td> <td> <?php print "$enrollmentCap"; ?> </td>
-    </tr>
-</table>
--->
+    <h1>Add Instructor Confirmation</h1>   
 <h3>The following instructor will be added</h3>
 <table>
-<?php include("confirmation_loop.php");?>
+    <?php include("confirmation_loop.php");?>
 </table>
 </body>
 </html>
