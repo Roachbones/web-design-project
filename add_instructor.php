d0cbb2251735e4bab13d1ebfb5ec6d5ca3525e15
spence <spencer.d.tenney@gmail.com>
2021-04-01 18:12:01 -0400

    removed display_form.js

--- add_instructor.php
+++ add_instructor.php
@@ -8,7 +8,6 @@
 <title>Add a Boring Instructor</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 <script src="form_validation.js"></script>
-<script src="display_form.js"></script>
 <?php session_start(); ?>
 <?php include("login_check.php"); ?>
 
