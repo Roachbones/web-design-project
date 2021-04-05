2eb7a7a55c7131106d1829384c39526007fd3b55
Vivian <26861054+Roachbones@users.noreply.github.com>
2021-04-05 13:04:39 -0400

    made the logout work better maybe?

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
     // unsets session variable for login and redirects to login_page.php
     // ending session, clearing $_SESSION array
+    
+    session_start();
     unset($_SESSION["login"]);
     unset($_SESSION["last_seen"]);
     
