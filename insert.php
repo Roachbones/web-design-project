45e28f199e0ce25abee127b9a88703205f73ea26
KadeHennacy <Kade.hennacy@gmail.com>
2021-04-22 15:01:17 -0400

    added page to insert values into db, added session variable to store form values

---
+++ insert.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>insert.php</title>
+    <?php session_start(); ?>
+    <?php include("login_check.php"); ?>
+</head>
+<body>
+    <h1>ayy</h1>
+<table>
+<?php
+// $arr = $_POST;
+var_dump($_SESSION["arr"]);
+?></table>
+</body>
+</html>
